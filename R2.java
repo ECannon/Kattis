8cfed0605ab54db1d967daa1b8635ef029d5040d
ECannon <33758070+ECannon@users.noreply.github.com>
2018-09-10 23:07:48 +0100

    R2

---
+++ R2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class R2 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int r1 = sc.nextInt();
+        int mean = sc.nextInt();
+        System.out.println((mean*2) - r1);
+    }
+
+}
