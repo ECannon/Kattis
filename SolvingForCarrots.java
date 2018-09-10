27a8b999b3380578fab5fb1ece302629bf586269
ECannon <33758070+ECannon@users.noreply.github.com>
2018-09-10 23:08:25 +0100

    Solving for Carrots

---
+++ SolvingForCarrots.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SolvingForCarrots 
+{
+    public static void main(String[] args) 
+    {   
+        Scanner sc = new Scanner(System.in);
+        
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        sc.next();
+        
+        for(int i =1;i<=num1;i++)
+        {
+            String s1 = sc.nextLine();
+        }
+        System.out.println(num2); 
+    }
+}
