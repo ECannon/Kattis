be4f2478696e990014a7f8abe4cee63fc0473ca0
ECannon <33758070+ECannon@users.noreply.github.com>
2018-09-11 14:19:37 +0100

    ARealChallenge

---
+++ ARealChallenge.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ARealChallenge {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+   
+        sc = new Scanner(System.in);    
+        double squareArea = sc.nextDouble();
+        double squarePerimeter = Math.sqrt(squareArea)*4;
+        System.out.println(squarePerimeter);  
+    }
+}
