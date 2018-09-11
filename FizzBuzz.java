82df37e7bd1a19ffb4121e477b0cf8855c069050
ECannon <33758070+ECannon@users.noreply.github.com>
2018-09-11 01:52:31 +0100

    FizzBuzz

---
+++ FizzBuzz.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class FizzBuzz 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int n = sc.nextInt();
+        
+        for(int i = 1; i <= n; i++)
+        {
+            if(i % x == 0 && i % y == 0)
+            {
+                System.out.println("FizzBuzz");
+            }
+            else if(i % x == 0)
+            {
+                System.out.println("Fizz");
+            }
+            else if(i % y == 0)
+            {
+                System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(i);
+            }
+        }   
+    }
+}
