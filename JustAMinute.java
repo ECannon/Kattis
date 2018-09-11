54275bf48fc5e46a091d37d414784f3b1d856aad
ECannon <33758070+ECannon@users.noreply.github.com>
2018-09-11 14:29:58 +0100

    JustAMinute

---
+++ JustAMinute.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class JustAMinute {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int cases = sc.nextInt();
+
+        int totalWaitingTime = 0;
+        int totalSeconds = 0;
+
+        for (int i = 1; i <= cases; i++) {
+            int minutesDisplayed = sc.nextInt();
+            int secondsWaited = sc.nextInt();
+
+            totalWaitingTime += minutesDisplayed;
+            totalSeconds += secondsWaited;
+        }
+
+        totalWaitingTime = totalWaitingTime * 60;
+
+        double value = (double) totalSeconds / totalWaitingTime;
+
+        value = (double) Math.round(value * 1000000000) / 1000000000;
+        
+        if(totalWaitingTime >= totalSeconds)
+        {
+            System.out.println("measurement error");
+        }
+        else
+        {
+            System.out.println(value);
+        }
+    }
+}
