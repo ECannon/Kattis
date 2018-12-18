116d4fb27fa96a6e1fb6be5098e991abb64af418
Eric Cannon <ericcannon@outlook.com>
2018-12-18 21:03:34 +0000

    Herman

---
+++ Herman.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Herman {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int r = sc.nextInt();
+		
+		System.out.println(Math.PI*(r*r));
+		System.out.println((double)(r*r)*2);
+	}
+
+}
