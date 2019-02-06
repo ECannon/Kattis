2883529e5e0670b09593d2f37e4843dadc6f349c
Eric Cannon <ericcannon@outlook.com>
2019-02-06 12:12:11 +0000

    Hissing Microphone

---
+++ HissingMicrophone.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class HissingMicrophone {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String s1 = sc.nextLine();
+		
+		if(s1.contains("ss")) System.out.println("hiss");
+		else System.out.println("no hiss");
+
+	}
+
+}
