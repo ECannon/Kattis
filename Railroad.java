40cfc6ab177341484fb714c5ee9ef5110c73ae57
Eric Cannon <ericcannon@outlook.com>
2019-02-06 12:46:59 +0000

    Railroad

---
+++ Railroad.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Railroad {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		if(y%2==0)System.out.println("possible");
+		else System.out.println("impossible");
+		
+	}
+
+}
